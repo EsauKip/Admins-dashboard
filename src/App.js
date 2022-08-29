40a3d732470d1ed5154bd9b089b61b3767055146
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-29 16:38:31 +0300

    add tool tip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import './index.css'
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./index.css";
 const App = () => {
   return (
-   <h1 className='underline text-3x1'>App</h1>
-  )
-}
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+    <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
+<TooltipComponent content="settings" position="top">
+  <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background:'blue',borderRadius:'50%'}}>
+    <FiSettings/>
+  </button>
 
-export default App
+</TooltipComponent>
+    </div>
+      </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
